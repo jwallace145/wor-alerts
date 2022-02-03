54bcc12fbfdbf16ef8c2dbd7cc4c7fd0809372fe
James  Wallace <jameswallace@Jamess-MBP.lan1>
2022-02-03 12:28:38 -0500

    Added DynamoDbGateway.get_users unit test to bump code coverage

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 test:
-	pipenv run pytest tests --cov=src --cov-report xml --verbose
+	pipenv run pytest tests --cov=src --cov-report term-missing --verbose
 
 run:
 	. ./secrets.sh
--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,13 @@ AWS_REGION = "us-east-1"
 
 
 @pytest.fixture
+def mock_dynamodb():
+    mock_dynamodb = moto.mock_dynamodb2()
+    mock_dynamodb.start()
+    return boto3.client("dynamodb", region_name=AWS_REGION)
+
+
+@pytest.fixture
 def mock_s3():
     mock_s3 = moto.mock_s3()
     mock_s3.start()
